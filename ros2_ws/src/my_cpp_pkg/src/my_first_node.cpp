3167593666a1b418b18d8d330384d096e8e5ba23
minje-KIM <minje617@naver.com>
2023-06-01 16:09:38 +0000

    made a cpp node

--- ros2_ws/src/my_cpp_pkg/src/my_first_node.cpp
+++ ros2_ws/src/my_cpp_pkg/src/my_first_node.cpp
@@ -1,12 +1,37 @@
 #include "rclcpp/rclcpp.hpp"
 
+class MyNode: public rclcpp::Node
+{
+    public:
+    MyNode(): Node("cpp_test"), counter_(0) // constructor -> node name, initialize counter to zero
+    {
+        RCLCPP_INFO(this->get_logger(), "Hello CPP Node Timer! ");
+        timer_ = this->create_wall_timer(std::chrono::seconds(1), //duration
+                            std::bind(&MyNode::timerCallback, this)); //callback function
+
+    }
+
+    private:
+    void timerCallback()
+    {
+        counter_++;
+        RCLCPP_INFO(this->get_logger(), "Hello %d", counter_);
+
+
+    }
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    int counter_;
+    
+};
+
+
+
 
 int main (int argc, char **argv)
 {
     rclcpp::init(argc, argv); //initialize
-
-    auto node = std::make_shared<rclcpp::Node>("cpp_test");
-    RCLCPP_INFO(node->get_logger(), "Hello CPP Node!");
+    auto node = std::make_shared<MyNode>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     
