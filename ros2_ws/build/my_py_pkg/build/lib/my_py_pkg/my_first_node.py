ff3f6a974aa796f59f4c26544705d7bf45bc8704
minje-KIM <minje617@naver.com>
2023-06-13 11:53:05 +0000

    service coding with python

---
+++ ros2_ws/build/my_py_pkg/build/lib/my_py_pkg/my_first_node.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node # To make a ROS2 node
+
+
+class MyNode(Node):
+    def __init__(self):
+        super().__init__("py_test_OOP")
+        self.counter_ = 0
+        self.get_logger().info("Hello ROS2 OOP_symlsdfsdfink")
+        self.create_timer(0.5, self.timer_callback) 
+        
+    def timer_callback(self): # callback function
+        self.counter_ += 1
+        self.get_logger().info("Hello Timer" + str(self.counter_))
+
+def main(args=None):
+    rclpy.init(args=args) # initialize ROS2 communication
+
+    node = MyNode()
+    rclpy.spin(node) # allow Node to continue to be alive 
+
+    rclpy.shutdown() # the last line for program
+
+
+if __name__ == "__main__":
+    main()
